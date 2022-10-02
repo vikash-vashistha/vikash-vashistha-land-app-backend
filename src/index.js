88333be40f9bdd3dfd512cabd4bf1367d7a20c33
Gourav1221 <90262724+Gourav1221@users.noreply.github.com>
2022-10-02 19:36:25 +0530

    added google oauth endpoint

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 var cors = require("cors");
 
 const connect = require("./configs/db");
-
+const passport = require("./configs/google.oatuh")
 const userController = require("./controllers/user.controller");
 const productController = require("./controllers/product.controller");
 const { register, login } = require("./controllers/auth.controller");
@@ -21,9 +21,34 @@ app.post("/login", login);
 app.use("/users", userController);
 app.use("/products", productController);
 
-app.listen(2345, async () => {
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile','email'] }));
+
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login',session:false}),
+  function(req, res) {
+    
+    // Successful authentication, redirect home.
+    console.log(req.user)
+    res.redirect('/');
+  });
+
+
+
+  app.get("/",(req,res)=>{
+    return res.send({msg:"hello"})
+  })
+
+
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, async () => {
   try {
     await connect();
+    console.log("connected to atlas")
   } catch (err) {
     console.error(err.message);
   }
