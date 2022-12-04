598310956339747b4ba2e3af0f13544a9354c98f
Vikash Vashistha <vikashvashistha.dev@gmail.com>
2022-12-04 16:23:59 +0530

    admin user add and delete

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -27,4 +27,37 @@ router.get("", authenticate, async (req, res) => {
   }
 });
 
+router.get("/admin", authenticate, async (req, res) => {
+  try {
+     let user;
+     let { name } = req.query;
+     console.log(name);
+     if (name) {
+       user = await User.find({ name: new RegExp(name, "i") })
+         .lean()
+         .exec();
+       // console.log(Cities, req.query.city);
+     } else {
+      user = await User.find().lean().exec();
+     }
+
+    return res.status(200).send(user);
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+});
+
+router.delete("/admin/:id", authenticate, async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  try {
+    const user = await User.deleteOne({_id: id}).lean().exec();
+    
+
+    return res.status(200).send(user);
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+});
+
 module.exports = router;
