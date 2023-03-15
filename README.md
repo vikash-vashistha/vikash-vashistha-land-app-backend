6560557d5d0de30fd22d6bf35f4618d8bd8b0cfe
Vikash Vashistha <vikashvashistha.dev@gmail.com>
2023-03-15 16:35:47 +0530

    readme file updated

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# vikash-vashistha-land-app-backend
+# CloneUrbanOutfitters
+
+Urban Outfitters is a lifestyle related retailer which specializes in selling apparel, clothing accessories and apartment products. It primarily targets teens and young adults who are interested in hipster subculture and alternative fashion..
+<a href="https://clone-urban-outfitters.vercel.app/" target="_blank">Click here for Live demo</a>
+
+## Tech Stack used:
+
+<ul>
+  <li>HTML</li>
+  <li>CSS</li>
+  <li>Javascript</li>
+  <li>Module.css</li>
+  <li>Node js</li>
+  <li>MongoDb</li>
+  <li>Express</li>
+  <li>Chakra UI</li>
+</ul>
+
+## Features:
+
+<ul>
+ <li>Search</li>
+  <li>filter and sorting</li>
+  <li>Add to cart</li>
+  <li>Login/Signup authentication</li>
+  <li>Responsive</li>
+<li>A customer can search for a city location and then see
+all available schemes.</li>
+
+<li>
+Within a specific scheme, the customer can see all of
+the available lands, check the availability filters by
+facility (water, electricity, roads, sewerage), and sort
+the lands by price.</li>
+<li>A customer can request to become a partner for the
+land, and after admin approval, can become a partner.</li>
+<li>A customer can browse the land and add any plot to his
+cart; after making payment, he can purchase that plot.</li>
+<li>
+The administrator has complete authority over the user,
+scheme, land, plots, transactions, and cart.</li>
+ 
+</ul>
+
+### Thanks 😊.
+
+![LandApp](/src/logo.png)
+![LandApp](/src/img1.png)
+![LandApp](/src/img2.png)
+![LandApp](/src/img3.png)
+
--- src/index.js
+++ src/index.js
@@ -1,56 +1,86 @@
-const express = require('express');
-const http = require('http');
-var cors = require('cors');
-// const { Server } = require("socket.io");
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
 const path = require("path");
 const shortid = require("shortid");
 const Razorpay = require("razorpay");
-const connect = require('./configs/db');
-const passport = require('./configs/google.oatuh');
+const connect = require("./configs/db");
+const passport = require("./configs/google.oatuh");
 // const userController = require('./controllers/user.controller');
-const productController = require('./controllers/product.controller');
-const { register, login, newToken } = require('./controllers/auth.controller');
-const transactionController = require('./controllers/transaction.controller');
-const schemeController = require('./controllers/scheme.controller')
-const landController = require('./controllers/land.controller')
-const userController = require('./controllers/user.controller')
-const cartController = require('./controllers/cart.controller')
-const chatController = require('./controllers/chat.controller')
-const requestController = require('./controllers/request.controller')
-const urlController = require('./controllers/url.controller')
+const productController = require("./controllers/product.controller");
+const { register, login, newToken } = require("./controllers/auth.controller");
+const transactionController = require("./controllers/transaction.controller");
+const schemeController = require("./controllers/scheme.controller");
+const landController = require("./controllers/land.controller");
+const userController = require("./controllers/user.controller");
+const cartController = require("./controllers/cart.controller");
+const chatController = require("./controllers/chat.controller");
+const requestController = require("./controllers/request.controller");
+const urlController = require("./controllers/url.controller");
 
 const app = express();
+// web sockit
+const httpServer = http.createServer(app);
+const { Server } = require("socket.io");
+const wss = new Server(httpServer);
+
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/index.html");
+// });
+
+let user_count = 0;
+let messages = [];
+wss.on("connection", (socket) => {
+  user_count++;
+  console.log("total user connected " + user_count);
+  socket.broadcast.emit("bmsg", "broad cast");
+  
+  socket.on("message", (msg) => {
+    console.log(msg);
+    messages.push(msg);
+    socket.emit("reply", "Hello from server");
+  });
+
+  
+  socket.emit("msgs", messages);
+  socket.on("disconnect", () => {
+    console.log("client disconnected");
+    user_count--;
+    wss.emit("user_count", user_count);
+  });
+  wss.emit("user_count", user_count);
+});
 
 app.use(cors());
 
 app.use(express.json());
 
 // /register
-app.post('/register', register);
+app.post("/register", register);
 // .login
-app.post('/login', login);
+app.post("/login", login);
 
-app.use('/user', userController);
-app.use('/products', productController);
-app.use('/scheme', schemeController);
-app.use('/land', landController);
-app.use('/cart', cartController);
-app.use('/chat', chatController);
-app.use('/request', requestController);
+app.use("/user", userController);
+app.use("/products", productController);
+app.use("/scheme", schemeController);
+app.use("/land", landController);
+app.use("/cart", cartController);
+app.use("/chat", chatController);
+app.use("/request", requestController);
 //transaction
 app.use("/transaction", transactionController);
 // url shortner
-app.use('url', urlController);
+app.use("url", urlController);
 
 app.get(
-  '/auth/google',
-  passport.authenticate('google', { scope: ['profile', 'email'] })
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
 );
 
 app.get(
-  '/auth/google/callback',
-  passport.authenticate('google', {
-    failureRedirect: '/login',
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/login",
     session: false,
   }),
   function (req, res) {
@@ -60,14 +90,14 @@ app.get(
     const token = newToken(req.user);
 
     // then return the user and the token
-    res.send({user: req.user, token });
+    res.send({ user: req.user, token });
     res.redirect("/");
   }
 );
 
-app.get('/', (req, res) => {
-  return res.send({ msg: 'hello' });
-});
+// app.get('/', (req, res) => {
+//   return res.send({ msg: 'hello' });
+// });
 
 // rojerpay
 const razorpay = new Razorpay({
@@ -134,25 +164,22 @@ app.post("/razorpay", async (req, res) => {
 
 const PORT = process.env.PORT || 2345;
 
-app.listen(PORT, async () => {
-  try {
-    await connect();
-    console.log('connected to atlas');
-  } catch (err) {
-    console.error(err.message);
-  }
-  console.log('listening on port 2345');
-});
-
-// const httpServer = http.createServer(app);
-// const wss = new Server(httpServer)
-
-// httpServer.listen(PORT, async () => {
+// app.listen(PORT, async () => {
 //   try {
 //     await connect();
-//     console.log("connected to atlas");
+//     console.log('connected to atlas');
 //   } catch (err) {
 //     console.error(err.message);
 //   }
-//   console.log("listening on port 2345");
+//   console.log('listening on port 2345');
 // });
+
+httpServer.listen(PORT, async () => {
+  try {
+    await connect();
+    console.log("connected to atlas");
+  } catch (err) {
+    console.error(err.message);
+  }
+  console.log("listening on port 2345");
+});
